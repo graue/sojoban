0957b88b2550a3a3d6d08546dea35853a8b52724
Scott Feeney <scott@oceanbase.org>
2014-01-11 18:50:26 -0500

    Use local images

--- src/sojoban/core.cljs
+++ src/sojoban/core.cljs
@@ -16,8 +16,7 @@
        #{:block :goal} "bag_goal"
        #{:player :goal} "man_goal"
        #{:wall} "block"}
-      (as-> x (str "http://www.ne.jp/asahi/ai/yoshio/sokoban/image/p_"
-                   x ".gif"))))
+      (as-> x (str "images/p_" x ".gif"))))
 
 (defn find-player
   "Return [row col] coordinates of the player on the board."
