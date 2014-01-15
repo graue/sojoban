a48900b5ce2ca5a0548f887444c67ad395dac607
Scott Feeney <scott@oceanbase.org>
2014-01-15 14:05:11 -0500

    Allow initial whitespace & omitted ending whitespace in levels
    For example, this is Microban level 15 (indented by 4 spaces):
    
             ###
        ######@##
        #    .* #
        #   #   #
        #####$# #
            #   #
            #####
    
    The previous regex removed initial whitespace, messing up the first and
    last two rows. Also, we ended up with a non-rectangular board because
    the implied space at the end of the first row is omitted from the level
    .txt file. make-rectangular adds that.

--- src/sojoban/read.cljs
+++ src/sojoban/read.cljs
@@ -17,6 +17,18 @@
    \* #{:block :goal}
    \+ #{:player :goal}})
 
+(defn make-rectangular
+  "Make the vector of vectors rectangular by padding each row with `fill`,
+  up to the length of the longest row."
+  [vov fill]
+  (let [max-length (apply max (map count vov))]
+    (mapv (fn [v]
+            (vec (take max-length
+                       (concat v (repeat fill)))))
+          vov)))
+
 (defn ascii-level-to-board [ascii]
-  (vov-map ascii-to-objset
-           (string/split ascii #"\n\s*")))
+  (as-> ascii x
+        (string/split x #"\n")
+        (vov-map ascii-to-objset x)
+        (make-rectangular x #{})))
